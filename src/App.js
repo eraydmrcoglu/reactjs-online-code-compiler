fb0941ffc52a74e2403491ae539fabb4839dc7c2
Eray Demircioğlu <c.eray@hotmail.com>
2023-06-26 13:25:22 +0300

    Code Editor

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Landing from './components/Landing';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Landing />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
